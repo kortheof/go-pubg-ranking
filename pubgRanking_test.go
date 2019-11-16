ecc9d6daa929c26578c917c0bef3f3498b2434de
Fanis Korlos <fanis.korlos@upstreamsystems.com>
2019-11-16 19:08:36 +0200

    Executing go linter in the testing file

--- pubgRanking_test.go
+++ pubgRanking_test.go
@@ -10,17 +10,18 @@ type ioTest struct {
 
 var mytest = []ioTest{
 	ioTest{
-		id: 1,
+		id:               1,
 		inCurrentLeaders: []int{110, 110, 80, 60, 60, 30, 25},
-		inMyScores: []int{3, 25, 50, 80, 90, 110, 120},
-		outMyRanking: []int{6,5,4,2,2,1,1},
+		inMyScores:       []int{3, 25, 50, 80, 90, 110, 120},
+		outMyRanking:     []int{6, 5, 4, 2, 2, 1, 1},
 	},
 }
+
 func TestLadderRanking(t *testing.T) {
-	for _,v := range mytest {
+	for _, v := range mytest {
 		out := ladderRanking(v.inCurrentLeaders, v.inMyScores)
 		equal := true
-		for i:= range out {
+		for i := range out {
 			if out[i] != v.outMyRanking[i] {
 				equal = false
 				break
