cb692f4bd884d309c3c2e31197cdb4a75ea61bb9
Fanis Korlos <fanis.korlos@upstreamsystems.com>
2019-11-16 19:19:18 +0200

    Adding more test cases

--- pubgRanking_test.go
+++ pubgRanking_test.go
@@ -15,11 +15,30 @@ var mytest = []ioTest{
 		inMyScores:       []int{3, 25, 50, 80, 90, 110, 120},
 		outMyRanking:     []int{6, 5, 4, 2, 2, 1, 1},
 	},
+	ioTest{
+		id:               2,
+		inCurrentLeaders: []int{100, 80, 80, 60},
+		inMyScores:       []int{50, 75, 105},
+		outMyRanking:     []int{4, 3, 1},
+	},
+	ioTest{
+		id:               3,
+		inCurrentLeaders: []int{100, 80, 80, 60},
+		inMyScores:       []int{102, 103},
+		outMyRanking:     []int{1, 1},
+	},
+	ioTest{
+		id:               4,
+		inCurrentLeaders: []int{100, 80, 80, 60},
+		inMyScores:       []int{20, 30},
+		outMyRanking:     []int{4, 4},
+	},
 }
 
 func TestLadderRanking(t *testing.T) {
 	for _, v := range mytest {
 		out := ladderRanking(v.inCurrentLeaders, v.inMyScores)
+
 		equal := true
 		for i := range out {
 			if out[i] != v.outMyRanking[i] {
