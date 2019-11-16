5233bcc1fcf2d1396e99fd7c4f6752f48f298f92
Fanis Korlos <fanis.korlos@upstreamsystems.com>
2019-11-16 18:17:57 +0200

    Adding solution for master branch

--- pubg-ranking.go
+++ pubg-ranking.go
@@ -8,7 +8,35 @@ import (
 // and calculates my ranking in the leaderboard.
 func ladderRanking(currentLeaders []int, myScores []int) []int {
 
-	return []int{6, 5, 4, 2, 2, 1, 1}
+	// Finding the ranking of the currentLeaders
+	currentLeadersRanking := make([]int, len(currentLeaders))
+	// currentLeaders is sorted descending, first element has the highest ranking
+	currentLeadersRanking[0] = 1
+	for i := 1; i < len(currentLeadersRanking); i++ {
+		if currentLeaders[i] == currentLeaders[i-1] {
+			currentLeadersRanking[i] = currentLeadersRanking[i-1]
+		} else {
+			currentLeadersRanking[i] = currentLeadersRanking[i-1] + 1
+		}
+
+	}
+
+	myScoresRanking := make([]int, len(myScores))
+	for i, v := range myScores {
+		for j := len(currentLeaders) - 1; j >= 0; j-- {
+			if v < currentLeaders[j] {
+				myScoresRanking[i] = currentLeadersRanking[j] + 1
+				break
+			} else if v == currentLeaders[j] {
+				myScoresRanking[i] = currentLeadersRanking[j]
+				break
+			}
+			myScoresRanking[i] = 1
+
+		}
+
+	}
+	return myScoresRanking
 
 }
 
